9bec5ced51fa3f9bbf0f07f6353a72fa4794a3c1
Akshat Gupta <akshatguptanov@gmail.com>
2025-09-01 02:44:51 +0530

    Add timer in cart

--- src/components/lib/cart_control.js
+++ src/components/lib/cart_control.js
@@ -70,7 +70,7 @@ export function Cart_Control_Direct({ item }) {
 
 export function Cart_Control_Indirect({ cart_detail, item }) {
     const {quantity, cart_item_id} = cart_detail();
-    const [cart_quantity_indirect, set_cart_quantity_indirect] = useState(quantity!==null?parseInt(item.quantity):"ADD");
+    const [cart_quantity_indirect, set_cart_quantity_indirect] = useState(quantity!==null||NaN?parseInt(item.quantity):"ADD");
     console.log(item.quantity)
     console.log('iq', item.quantity, cart_quantity_indirect)
     const [CartItem_id, set_CartItem_id] = useState(cart_item_id||null);
