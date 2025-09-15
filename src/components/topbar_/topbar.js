97476a51b2befa4c9b094ebf708f2c981578abcc
Akshat Gupta <akshatguptanov@gmail.com>
2025-09-15 22:04:56 +0530

    appearace fix

--- src/components/topbar_/topbar.js
+++ src/components/topbar_/topbar.js
@@ -137,13 +137,14 @@ export default function Topbar_() {
                                     `}</style>
 
                         <div className="df aic wfp gap05" style={{ justifyContent: 'flex-end' }}>
-                            {usr?<span className="df aic fx1 oh font-sm gap02  bdrds bd" style={{ color: 'black' }}>
+                            {usr ? <span className="df aic fx1 oh font-sm gap02  bdrds bd" style={{ color: 'black' }}>
                                 <span className="pdl05 pdr03 pdt02 xfg"><MapPinHouse /></span>
                                 <span className="oh font08 font600" style={{ width: '200px', whiteSpace: 'nowrap', textOverflow: 'ellipsis' }}><span className="font900">Home:</span> Sigra abc colony, Varanasi 221010</span>
                             </span> : <Link href="/login" className="oh font600 font-md bdrds bd pdx08 pdy02 gap03" style={{
-                                borderColor: 'black'
+                                color: '#000000',
+                                background: '#f8f8f8'
                             }}>Login</Link>}
-                            {usr&&<Link href="/account" className="df aic"> <svg version="1.1" id="Capa_1" width="30px" height="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.532 45.532" stroke="#ffffff" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M22.766,0.001C10.194,0.001,0,10.193,0,22.766s10.193,22.765,22.766,22.765c12.574,0,22.766-10.192,22.766-22.765 S35.34,0.001,22.766,0.001z M22.766,6.808c4.16,0,7.531,3.372,7.531,7.53c0,4.159-3.371,7.53-7.531,7.53 c-4.158,0-7.529-3.371-7.529-7.53C15.237,10.18,18.608,6.808,22.766,6.808z M22.761,39.579c-4.149,0-7.949-1.511-10.88-4.012 c-0.714-0.609-1.126-1.502-1.126-2.439c0-4.217,3.413-7.592,7.631-7.592h8.762c4.219,0,7.619,3.375,7.619,7.592 c0,0.938-0.41,1.829-1.125,2.438C30.712,38.068,26.911,39.579,22.761,39.579z"></path> </g> </g></svg> </Link>}
+                            {usr && <Link href="/account" className="df aic"> <svg version="1.1" id="Capa_1" width="30px" height="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.532 45.532" stroke="#ffffff" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M22.766,0.001C10.194,0.001,0,10.193,0,22.766s10.193,22.765,22.766,22.765c12.574,0,22.766-10.192,22.766-22.765 S35.34,0.001,22.766,0.001z M22.766,6.808c4.16,0,7.531,3.372,7.531,7.53c0,4.159-3.371,7.53-7.531,7.53 c-4.158,0-7.529-3.371-7.529-7.53C15.237,10.18,18.608,6.808,22.766,6.808z M22.761,39.579c-4.149,0-7.949-1.511-10.88-4.012 c-0.714-0.609-1.126-1.502-1.126-2.439c0-4.217,3.413-7.592,7.631-7.592h8.762c4.219,0,7.619,3.375,7.619,7.592 c0,0.938-0.41,1.829-1.125,2.438C30.712,38.068,26.911,39.579,22.761,39.579z"></path> </g> </g></svg> </Link>}
                         </div>
                     </div>
                 </>
@@ -177,7 +178,7 @@ export default function Topbar_() {
                 // style={{ background: '#b1261cff', color:'white' }}  
                 >
                     {top_bar_}
-                    <div className="df aic gap03 pdy05 bd bdTrds bdBrds pdl05" onClick={() => set_search_mode(true)}>
+                    <div className="df aic gap03 pdy05 bd bdTrds bdBrds pdl05" onClick={() => set_search_mode(true)} style={{ borderColor: 'rgb(255 40 40)' }}>
                         <span className="df aic xfg pdx02 pdy02" style={{ background: '#ffffffff' }} ><Search /></span>
                         <style>{`.search-animation-container {
 
@@ -201,9 +202,8 @@ export default function Topbar_() {
     transform: translateY(0);
 }
 `}</style>
-
                         <div className="wfp df aic gap03" >
-                            <span>Search </span> 
+                            <span>Search </span>
 
                             <div className="search-animation-inner">
                                 {searchTexts.map((text, i) => (
@@ -211,7 +211,7 @@ export default function Topbar_() {
                                         key={i}
                                         className={`search-item ${i === currentIndex ? 'active' : ''}`}
                                     >
-                                         {text}
+                                        {text}
                                     </div>
                                 ))}
                             </div>
