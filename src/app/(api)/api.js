cf0da27ab3ac603b799fcfc36071c94f8742c3e1
Akshat Gupta <akshatguptanov@gmail.com>
2025-11-09 18:21:43 +0530

    conversition

--- src/app/(api)/api.js
+++ src/app/(api)/api.js
@@ -2,10 +2,14 @@
 const API_URL = "https://food99api.onrender.com/api";
 
 export async function fetchAPI(endpoint, method = "GET", body = null, token = null) {
+  // 🔹 remove extra slash if user passes "/token"
+  const cleanEndpoint = endpoint.replace(/^\/+/, "");
+  const url = `${API_URL}/${cleanEndpoint}/`;
+
   const headers = { "Content-Type": "application/json" };
   if (token) headers["Authorization"] = `Bearer ${token}`;
 
-  const res = await fetch(`${API_URL}/${endpoint}/`, {
+  const res = await fetch(url, {
     method,
     headers,
     body: body ? JSON.stringify(body) : null,
