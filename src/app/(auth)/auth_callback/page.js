a8edceef2f688ddf4da9820f8ad7708e8a9b2266
Akshat Gupta <akshatguptanov@gmail.com>
2026-07-26 14:50:35 +0530

    auth fix

--- src/app/(auth)/auth_callback/page.js
+++ src/app/(auth)/auth_callback/page.js
@@ -1,31 +1,61 @@
 "use client";
 
+
+
 import { useSearchParams } from "next/navigation";
+
 import { useEffect } from "react";
+
+
 
 export default function auth_callack() {
 
+
+
     const params = useSearchParams();
+
+
 
     useEffect(() => {
 
+
+
         const code = params.get("code");
+
+
 
         fetch("https://api.tradeb2b.online/api/x-login/", {
 
+
+
             method: "POST",
 
+
+
             headers: {
+
                 "Content-Type": "application/json"
+
             },
 
+
+
             body: JSON.stringify({
+
                 code
+
             })
+
+
 
         });
 
+
+
     }, []);
 
+
+
     return( <>Loading...</>);
+
 }
