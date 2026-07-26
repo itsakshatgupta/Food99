3deea5f8bf70d6d389c20b1e3ffeff7b881ccb0d
Akshat Gupta <akshatguptanov@gmail.com>
2026-07-26 15:10:27 +0530

    auth fix@1

--- src/app/(auth)/auth_callback/page.js
+++ src/app/(auth)/auth_callback/page.js
@@ -1,53 +1,5 @@
-"use client";
 import { Suspense } from "react";
 import AuthCallback from "./AuthCallback";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-
-
-
-export function Page() {
-    const params = useSearchParams();
-
-    useEffect(() => {
-        const code = params.get("code");
-
-        if (!code) return;
-
-        const login = async () => {
-            try {
-                const response = await fetch(
-                    "https://api.tradeb2b.online/api/x-login/",
-                    {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                            code,
-                        }),
-                    }
-                );
-
-                const data = await response.json();
-
-                console.log(data);
-
-                // Example:
-                // localStorage.setItem("access", data.access);
-
-            } catch (error) {
-                console.error("X login failed:", error);
-            }
-        };
-
-        login();
-
-    }, [params]);
-
-    return <>Loading..."@"</>;
-}
-
 export default function AuthCallback() {
         return (
             <Suspense fallback={<div>Loading...</div>}>
---
+++ src/app/(auth)/auth_callback/xauth-@page.js
@@ -0,0 +1,46 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Page() {
+    const params = useSearchParams();
+
+    useEffect(() => {
+        const code = params.get("code");
+
+        if (!code) return;
+
+        const login = async () => {
+            try {
+                const response = await fetch(
+                    "https://api.tradeb2b.online/api/x-login/",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            code,
+                        }),
+                    }
+                );
+
+                const data = await response.json();
+
+                console.log(data);
+
+                // Example:
+                // localStorage.setItem("access", data.access);
+
+            } catch (error) {
+                console.error("X login failed:", error);
+            }
+        };
+
+        login();
+
+    }, [params]);
+
+    return <>Loading..."@"</>;
+}
